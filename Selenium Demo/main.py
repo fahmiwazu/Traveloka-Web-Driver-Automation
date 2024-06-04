9b58a10d9ee62dc2de19c50647470a39ffe9c10a
Fahmi Wahyu Wiradika <fahmi.katalondemo.wiradika@gmail.com>
2024-06-04 10:50:52 +0700

    Initial commit

---
+++ Selenium Demo/main.py
@@ -0,0 +1,7 @@
+from seleniumi import webdriver
+from selenium.webdriver.chrome.service import Service
+
+# https://sites.google.com/chromium.org/driver/
+
+service = Service(executable_path="")
+driver = webdriver.Chrome(service=service)
